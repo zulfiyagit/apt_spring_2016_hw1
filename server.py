9660144710192b893766b0f911b3aa1aaa07e623
Zulfiya <yusupova.zulfiya@gmail.com>
2016-02-09 15:45:48 +0000

    Zulfiya

--- server.py
+++ server.py
@@ -6,8 +6,22 @@ import os
 
 @route('/')
 def index():
-    return {"usd": 378, "eur": 415, "rub": 4.82}
-
+    URL = "http://public.mig.kz/"
+    resp = requests.get(URL)
+    bs4 = BeautifulSoup(resp.content, "html.parser")
+    sell = []
+    i=0
+    table = bs4.find('table')
+    for tr in table.find_all('tr'):
+        for tag in tr.find_all('td', {'class': 'sell delta-neutral'}):
+            
+            sell.append(tag.string)
+            i+=1
+        
+    return {"USD": sell[0], "EUR": sell[1], "RUB": sell[2], "KGS": sell[3], "GBP": sell[4], "CNY": sell[5], "CHF": sell[6]}
+        
+        
+    
 
 URL = "https://github.com/giAtSDU/apt_spring_2016_hw1"
 
@@ -22,3 +36,4 @@ def forks():
     return {"forks": res}
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
